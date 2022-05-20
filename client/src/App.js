177d39d9c28886dfc7f1919ba6812b588d2de17c
Virochaan <39714123+VirochaanRG@users.noreply.github.com>
2022-05-20 16:25:55 -0400

    crating posts functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles'
+import {getPosts} from './actions/posts'
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return ( 
         <Container maxidth="lg">
@@ -16,7 +23,7 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" align-items="stretch">
+                    <Grid container justifyContent="space-between" align-items="stretch">
                         <Grid item xs={12} sm={7}>
                             <Posts/>
                         </Grid>
